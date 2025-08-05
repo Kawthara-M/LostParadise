330330dcd6f6e92d217756b96798ba58a15c46b4
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-08-05 11:09:12 +0300

    update server

--- server.js
+++ server.js
@@ -1,54 +1,46 @@
 // imports
-const express = require('express')
-require('dotenv').config()
-const path = require('path')
-
+const express = require("express")
+require("dotenv").config()
+const path = require("path")
+const cors = require("cors")
 
 // Initialize app
 const app = express()
 
 // Database Configuration
-const mongoose = require('./config/db')
+const mongoose = require("./config/db")
 
 // set Port Configuration
 const port = process.env.PORT ? process.env.PORT : 3000
 
 // Require MiddleWares
-const methodOverride = require('method-override')
-const morgan = require('morgan')
-
+const methodOverride = require("method-override")
+const morgan = require("morgan")
 
 // Require passUserToView & isSignedIn middlewares
 
-
 // use MiddleWares
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(morgan("dev"))
 app.use(express.static(path.join(__dirname, "public")))
 
-
-
 // Require Routers
 
-
-
 // Root Route
-app.get('/', (req, res) => {
-  res.send('Your app is connected . . . ')
+app.get("/", (req, res) => {
+  res.send("Your app is connected . . . ")
 })
 
 // Require Routers
 const gamesRouter = require("./routes/games")
 
-
 // use Routers
-app.use("/games",gamesRouter)
-
+app.use("/games", gamesRouter)
 
 // Listener
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
 })
-
